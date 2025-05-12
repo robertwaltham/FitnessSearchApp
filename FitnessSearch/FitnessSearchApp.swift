bdfdb280975c07cffaeb8492b950f08f47e9fec5
Robert Waltham <rob@blockoftext.com>
2025-05-12 14:59:12 -0700

    Initial Commit

---
+++ FitnessSearch/FitnessSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessSearchApp.swift
+//  FitnessSearch
+//
+//  Created by Robert Waltham on 2025-05-12.
+//
+
+import SwiftUI
+
+@main
+struct FitnessSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
